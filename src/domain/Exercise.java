5d925b5e4c873482fb87a312916b40b0ffca82f4
Alexander Babich <alexander.taurus@gmail.com>
2020-06-05 17:34:01 +0300

    Initial commit

---
+++ src/domain/Exercise.java
@@ -0,0 +1,7 @@
+package domain;
+
+public class Exercise {
+    public static float Calculate(int R){
+        return (float) (2*Math.PI*Math.sqrt(R));
+    }
+}
---
+++ src/test/TestResult.java
@@ -0,0 +1,12 @@
+package test;
+
+import domain.Exercise;
+
+public class TestResult {
+
+    public static void main(String[] args) {
+
+        int R=10;
+        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    }
+}
